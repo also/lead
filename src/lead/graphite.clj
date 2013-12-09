a0a30ee0a20fa829cbcb46ad6221df741db98e01
Ryan Berdeen <ryan@ryanberdeen.com>
2013-12-09 02:38:19 -0500

    graphite queries

--- src/lead/graphite.clj
+++ src/lead/graphite.clj
@@ -16,7 +16,19 @@
 
 (defrecord GraphiteConnector [url]
   Connector
-  (query [this pattern] nil)
+  (query [this pattern]
+    (let [url (str (:url this) "/metrics/find")
+          response (http/get url {:as :json
+                                  :query-params {"query" pattern
+                                                 "format" "completer"}})]
+      (map (fn [result]
+             {:name (let [name (:path result)]
+                      (if (.endsWith name ".")
+                        (.substring name 0 (- (.length name) 1))
+                        name))
+              :is-leaf (= "1" (:is_leaf result))})
+           (-> response :body :metrics))))
+
   (load-serieses [this targets {:keys [start end]}]
     (let [url (str (:url this) "/render/")
           response (http/get url {:as :json
