8d717e3951b291e632d1f220c587585c32506b98
Ryan Berdeen <ryan@ryanberdeen.com>
2013-12-09 23:51:46 -0500

    don't :use functions

--- src/lead/main.clj
+++ src/lead/main.clj
@@ -1,13 +1,13 @@
 (ns lead.main
   (:gen-class)
-  (:use lead.functions)
   (:require [lead.api :refer [create-routes add-routes *routes*]]
             [lead.jetty-api :as server]
-            [lead.connector :refer [set-connector] :as conn]))
+            [lead.connector :refer [set-connector] :as conn]
+            [lead.functions :refer [register-fns-from-namespace] :as fns]))
 
 (defn -main [& [port config-file]]
   (binding [*ns* (the-ns 'lead.main)
-            *fn-registry* (create-registry)
+            fns/*fn-registry* (fns/create-registry)
             conn/*connector* (conn/init-connector)
             *routes* (create-routes)]
     (load-file config-file)
