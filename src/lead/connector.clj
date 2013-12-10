114c68618519fce4cd49c501f34eb06b4f0d83fa
Ryan Berdeen <ryan@ryanberdeen.com>
2013-12-10 00:11:57 -0500

    connector list

--- src/lead/connector.clj
+++ src/lead/connector.clj
@@ -11,3 +11,15 @@
 (defn set-connector
   [connector]
   (reset! *connector* connector))
+
+(defrecord ConnectorList [connectors]
+  Connector
+  (query [this pattern]
+    (flatten (pmap #(query % pattern) (:connectors this))))
+
+  (load-serieses [this targets opts]
+    (flatten (pmap #(load-serieses % targets opts) (:connectors this)))))
+
+(defn connector-list
+  [& connectors]
+  (->ConnectorList (flatten connectors)))
