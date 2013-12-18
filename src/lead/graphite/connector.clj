d5eb189ecff37533f3e37fa90f713079a43dfa4a
Ryan Berdeen <ryan@ryanberdeen.com>
2013-12-18 00:45:03 -0500

    filter out graphite-added wildcards

--- src/lead/graphite/connector.clj
+++ src/lead/graphite/connector.clj
@@ -3,7 +3,8 @@
            (com.google.common.base Charsets)
            (java.util Collections))
   (:require [clj-http.client :as http]
-            [lead.connector :refer [Connector query load-serieses connector-list]]))
+            [lead.connector :refer [Connector query load-serieses connector-list]]
+            [lead.matcher :refer [segment-matcher]]))
 
 (defn graphite-json->serieses [targets]
     (map (fn [target]
@@ -17,6 +18,21 @@
                   :values (map first (:datapoints target))))
          targets))
 
+(defn- transform-find-response
+  [pattern response]
+  (let [last-segment-matches (-> pattern (.split "\\.") last segment-matcher)]
+    ; graphite tacks on "*" to the end of every query:
+    ; https://github.com/graphite-project/graphite-web/blob/0.9.12/webapp/graphite/metrics/views.py#L157-L158
+    (filter (fn [result] (-> result :name (.split "\\.") last last-segment-matches))
+            (map (fn [result]
+                   {:name    (let [name (:path result)]
+                               (if (.endsWith name ".")
+                                 (.substring name 0 (- (.length name) 1))
+                                 name))
+                    :is-leaf (= "1" (:is_leaf result))})
+                 (-> response :metrics))))
+  )
+
 (defrecord GraphiteConnector [url]
   Connector
   (query [this pattern]
@@ -24,17 +40,7 @@
           response (http/get url {:as :json
                                   :query-params {"query" pattern
                                                  "format" "completer"}})]
-
-      ; TODO "completer" is the closest format to what we need, but it tacks on
-      ; "*" to the end of every query: https://github.com/graphite-project/graphite-web/blob/0.9.12/webapp/graphite/metrics/views.py#L157-L158
-      ; we should filter the results on the last path segment
-      (map (fn [result]
-             {:name (let [name (:path result)]
-                      (if (.endsWith name ".")
-                        (.substring name 0 (- (.length name) 1))
-                        name))
-              :is-leaf (= "1" (:is_leaf result))})
-           (-> response :body :metrics))))
+      (transform-find-response pattern (:body response))))
 
   (load-serieses [this targets {:keys [start end]}]
     (let [url (str (:url this) "/render/")
