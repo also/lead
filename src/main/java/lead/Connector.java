cc55fa10d8aff8b9eb0729ad721249c739ded665
Ryan Berdeen <ryan@ryanberdeen.com>
2014-01-15 23:10:16 -0500

    oh, right, java

--- src/main/java/lead/Connector.java
+++ src/main/java/lead/Connector.java
@@ -3,7 +3,7 @@ package lead;
 import java.util.List;
 
 public interface Connector {
-  List<TreeNode> find(String pattern);
+  List<TreeNode> find(String pattern) throws Exception;
 
-  List<Series> load(List<String> targets, LoadOptions options);
+  List<Series> load(List<String> targets, LoadOptions options) throws Exception;
 }
