ec8368f38662b72d92d95786109ebec422a7a5be
Ryan Berdeen <ryan@ryanberdeen.com>
2014-02-04 18:54:46 -0500

    update java load interface

--- src/main/java/lead/Connector.java
+++ src/main/java/lead/Connector.java
@@ -5,5 +5,5 @@ import java.util.List;
 public interface Connector {
   List<TreeNode> find(String pattern) throws Exception;
 
-  List<Series> load(List<String> targets, LoadOptions options) throws Exception;
+  List<Series> load(String target, LoadOptions options) throws Exception;
 }
