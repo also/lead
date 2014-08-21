5a38d5be457823c4487d38072c007b6c55090020
Ryan Berdeen <ryan@ryanberdeen.com>
2014-08-20 23:02:31 -0400

    opentsdb return format, force-interval function

--- src/main/clojure/lead/opentsdb/functions.clj
+++ src/main/clojure/lead/opentsdb/functions.clj
@@ -2,7 +2,9 @@
   (:require [lead.main]
             [lead.core :refer [*configuration*]]
             [lead.functions :refer [leadfn]]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            [clojure.string :as string]
+            [lead.builtin-functions :refer [map-serieses]]))
 
 (defn set-base-url [base-url]
   (lead.main/update-config assoc-in [:opentsdb :base-url] base-url))
@@ -11,17 +13,46 @@
 (defn set-query-opts [query-opts]
   (lead.main/update-config assoc-in [:opentsdb :query-opts] query-opts))
 
+(defn opentsdb-result->series
+  [{:keys [start end]} opentsdb-result]
+  {:name (str (:metric opentsdb-result)
+              (if-let [tags (seq (:tags opentsdb-result))]
+                (str \{
+                     (string/join \, (map (fn [[k v]] (str (name k) \= v)) tags))
+                     \})))
+   :start start
+   :end end
+   :values (:dps opentsdb-result)
+   :meta {:opentsdb (dissoc opentsdb-result :dps)}})
+
 (leadfn
   ^{:args "s"
     :complicated true}
   opentsdb
   [opts metric]
   (let [config (:opentsdb *configuration*)]
-    (:body (http/get (str (:base-url config) "/api/query") (merge (:query-opts config)
-                                                            {:as           :json
-                                                             :query-params {"start"  (:start opts)
-                                                                            "end"    (:end opts)
-                                                                            "m"      metric
-                                                                            "arrays" true}}))))
-  )
+    (map (partial opentsdb-result->series opts)
+         (:body (http/get (str (:base-url config) "/api/query")
+                          (merge (:query-opts config)
+                                 {:as           :json
+                                  :query-params {"start"  (:start opts)
+                                                 "end"    (:end opts)
+                                                 "m"      metric
+                                                 "arrays" true}}))))))
 
+(leadfn
+  ^{:args "Ii"
+    :aliases ["forceInterval"]}
+  force-interval [serieses interval]
+  (map
+    (fn [series]
+      (let [start (:start series)
+            bucket-count (quot (- (:end series) start) interval)
+            buckets (make-array Number bucket-count)]
+        (doseq [[timestamp value] (:values series)]
+          (let [bucket-index (quot (- timestamp start) interval)]
+            (if (and (>= bucket-index 0)
+                     (< bucket-index bucket-count))
+              (aset buckets bucket-index value))))
+        (assoc series :step interval :values (seq buckets))))
+    serieses))
