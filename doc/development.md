43f28064965432547b22a1f000155e076b89c327
Ryan Berdeen <ryan@ryanberdeen.com>
2015-01-03 16:23:07 -0500

    remove cljs profile

--- doc/development.md
+++ doc/development.md
@@ -8,9 +8,7 @@ npm install
 
 before running tests.
 
-There's a separate Leiningen profile for ClojureScript tests to add the dependency on instaparse-cljs.
-
 ```
 lein cljx
-lein with-profile cljs cljsbuild test
+lein cljsbuild test
 ```
--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
     [ring/ring-jetty-adapter "1.2.1"]
     [ring/ring-json "0.2.0"]
     [compojure "1.1.5"]
-    [instaparse "1.2.8"]
+    [com.lucasbradstreet/instaparse-cljs "1.3.4.2"]
     [org.clojure/tools.logging "0.2.6"]
     [com.cemerick/clojurescript.test "0.2.1"]
     [joda-time/joda-time "2.3"]
@@ -21,6 +21,7 @@
   :main ^:skip-aot lead.main
   :aot [lead.matcher]
   :plugins [[com.keminglabs/cljx "0.5.0"]
+            [lein-cljsbuild "1.0.4"]
             [codox "0.6.6"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
@@ -38,22 +39,19 @@
                   {:source-paths ["src/test/cljx"]
                    :output-path "target/generated/test-cljs"
                    :rules :cljs}]}
+  :cljsbuild {:builds [{:source-paths ["target/generated/cljs" "target/classes"]
+                        :compiler {:optimizations :none
+                                   :pretty-print true
+                                   :output-dir "target/js"
+                                   :output-to "target/js/index.js"}}
+                       {:source-paths ["target/generated/cljs" "target/classes" "target/generated/test-cljs" "target/test-classes"]
+                        :compiler {:optimizations :none
+                                   :pretty-print true
+                                   :output-dir "target/test-js"
+                                   :output-to "target/test-js/index.js"}}]
+              :test-commands {"node" ["./node_modules/coffee-script/bin/coffee" "test/coffee/run_clojure_tests.coffee"]}}
   :aliases {"cleantest" ["do" "clean," "cljx" "once," "test"]}
   ;; need this to work with leiningen 2.3.1 used on travis-ci
   ;; cljx should probably support %s or another way to reference project configuration
   :profiles {:test {:target-path "target"
-                    :test-paths ["target/test-classes"]}
-             :cljs {:cljsbuild {:builds [{:source-paths ["target/generated/cljs" "target/classes"]
-                                          :compiler {:optimizations :none
-                                                     :pretty-print true
-                                                     :output-dir "target/js"
-                                                     :output-to "target/js/index.js" }}
-                                         {:source-paths ["target/generated/cljs" "target/classes" "target/generated/test-cljs" "target/test-classes"]
-                                          :compiler {:optimizations :none
-                                                     :pretty-print true
-                                                     :output-dir "target/test-js"
-                                                     :output-to "target/test-js/index.js"}}]
-                                :test-commands {"node" ["./node_modules/coffee-script/bin/coffee" "test/coffee/run_clojure_tests.coffee"]}}
-                    ; TODO this should replace instaparse dependency
-                    :dependencies [[com.lucasbradstreet/instaparse-cljs "1.3.4.2"]]
-                    :plugins [[lein-cljsbuild "1.0.4"]]}})
+                    :test-paths ["target/test-classes"]}})
