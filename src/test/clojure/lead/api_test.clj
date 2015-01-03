cf0cff4b4a4672ad99632752d377695d6cb7d5f9
Ryan Berdeen <ryan@ryanberdeen.com>
2015-01-03 13:52:16 -0500

    simple eval-targets tests

--- src/test/clojure/lead/api_test.clj
+++ src/test/clojure/lead/api_test.clj
@@ -1,6 +1,9 @@
 (ns lead.api-test
   (:require [clojure.test :refer [deftest is]]
-            [lead.api :as api]))
+            [lead.api :as api]
+            [lead.core :as core]
+            [lead.functions :as fns]
+            [cheshire.core :as json]))
 
 (def opts {:now 1420088400
            :start 1420002000
@@ -15,3 +18,16 @@
     (is (= now (:now opts)))
     (is (= yesterday (:start opts)))
     (is (= now (:end opts)))))
+
+(deftest test-eval-targets-no-targets
+  (let [result (api/eval-targets [] opts)]
+    (is (= [] result))))
+
+(deftest test-eval-targets-missing-function
+  (binding [core/*context* (atom (core/create-context))
+            fns/*fn-registry* {}]
+    (let [result (api/eval-targets ["missingFunction()"] opts)
+          exceptions (:exceptions @core/*context*)]
+      (is (= [{:target "missingFunction()", :result nil}] result))
+      (is (= 1 (count exceptions)))
+      (is (= "missingFunction" (-> exceptions first ex-data :name))))))
