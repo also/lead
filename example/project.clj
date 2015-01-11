d9eb58985aa9d498e4abd1c6162e846034742d82
Ryan Berdeen <ryan@ryanberdeen.com>
2015-01-10 19:57:28 -0500

    update clojure version in example

--- example/project.clj
+++ example/project.clj
@@ -1,5 +1,5 @@
 (defproject lead-example "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.ryanberdeen/lead "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot lead.main)
